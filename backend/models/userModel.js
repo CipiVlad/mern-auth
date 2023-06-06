f0e7c364612f87f4b686efacd1b2a4574f856f68
CipiVlad <102032121+CipiVlad@users.noreply.github.com>
2023-06-07 01:22:51 +0200

    server-side: register a user; jsonwebtoken and HttpOnly Cookie comming up next

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+},
+
+    {
+        timestamps: true
+    }
+);
+
+//  'pre' is a mongoose hook --> meaning before save the userSchema ... do this
+userSchema.pre('save', async function (next) {
+
+    // if the password isn't changed in anyway, move on --> next();
+    if (!this.isModified('password')) {
+        next();
+    }
+    // if the password is modified:
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
